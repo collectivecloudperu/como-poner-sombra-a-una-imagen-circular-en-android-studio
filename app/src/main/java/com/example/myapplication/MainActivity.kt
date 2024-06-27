043f7baede663bc5b66df0f4e7159fd8f181c3c4
collectivecloudperu <collectivecloudperu@gmail.com>
2024-06-27 00:43:51 -0500

    Cambios

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
